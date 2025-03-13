9e6250b6522c35b197220383548c7287fcf58847
Gergo Gyori <gergo.gyori@gmail.com>
2025-03-13 17:51:19 +0100

    bam bam

--- styles.js
+++
@@ -1,8 +0,0 @@
-// Basic major styles array - loaded from the CSV file
-const basicMajorStyles = [
-    "House", "Techno", "Tech House", "Progressive House", "Deep House",
-    "Minimal", "Drum n Bass", "Disco", "Trance", "Electro", "Ambient",
-    "Experimental", "Progressive Trance", "Hard Techno", "Dubstep",
-    "Funk", "Downtempo", "Breakbeat", "Soul", "UK Garage", "Jungle",
-    "Breaks", "Synth-pop", "Dub Techno", "Jazz"
-];
